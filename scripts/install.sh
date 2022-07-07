19909b238d1f8cdd523712404da4d86f114ba7f7
goldfinger <60414807+g0Idfinger@users.noreply.github.com>
2022-07-07 14:35:01 -0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,21 +4,16 @@ if [ `whoami` = root ]; then
 	exit
 fi
 #updates ubuntu
+
 sudo apt update && sudo apt upgrade -y
 #installs prerequisites
-sudo apt install apt-transport-https ca-certificates curl software-properties-common jq ruby-dev -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common ftp-upload ruby-dev gnupg lsb-release -y
 sudo gem install xkpassword
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce -y
-#install docker compose v2
-# create the docker plugins directory if it doesn't exist yet
-mkdir -p /home/$USER/.docker/cli-plugins
-# download the CLI into the plugins directory
-curl -sSL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o /home/$USER/.docker/cli-plugins/docker-compose
-# make the CLI executable
-chmod +x /home/$USER/.docker/cli-plugins/docker-compose
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 #Build out .env file
 puid=$(id -u)
 echo $puid
@@ -30,7 +25,7 @@ echo "USERDIR=$HOME" >> ../.env
 dir=${PWD%/*}
 echo "DOCKERDIR=$dir" >> ../.env
 echo "VELOX_ROLE=administrator" >> ../.env
-read -p 'Domain Root,example mydomain.com: ' domainroot
+read -p 'Domain Root,example example.com: ' domainroot
 echo "DOMAINNAME=$domainroot" >> ../.env
 read -p 'Cloudflare Email:' cfemail
 echo "CLOUDFLARE_EMAIL=$cfemail" >> ../.env
@@ -38,14 +33,21 @@ read -p 'CLOUDFLARE TOKEN:' cftoken
 echo "CLOUDFLARE_TOKEN=$cftoken" >> ../.env
 read -p 'CLOUDFLARE API KEY: ' cfapi
 echo "CLOUDFLARE_API_KEY=$cfapi" >> ../.env
+#read -p 'Velo Username:' veloun
+#echo "VELOX_USER=$veloun" >> ../.env
+#read -p 'Velo Password: ' velopw
+#echo "VELOX_PASSWORD=$velopw" >> ../.env
 #add user to docker group
 sudo usermod -aG docker ${USER}
 sudo chmod 666 /var/run/docker.sock
 #create folder structure
-mkdir ../containers/traefik2/acme
-touch ../containers/traefik2/acme/acme.json
-chmod 600 ../containers/traefik2/acme/acme.json
-touch ../containers/traefik2/traefik.log
-docker network create --gateway 192.168.90.1 --subnet 192.168.90.0/24 t2_proxy
+mkdir ../appdata/traefik2/acme
+touch ../appdata/traefik2/acme/acme.json
+chmod 600 ../appdata/traefik2/acme/acme.json
+touch ../appdata/traefik2/traefik.log
+#docker network create --gateway 192.168.90.1 --subnet 192.168.90.0/24 t2_proxy
 cd ..
+mkdir velo-staging/output/
+cd $dir
+cp docker-compose.yml.bak docker-compose.yml
 docker compose up -d
