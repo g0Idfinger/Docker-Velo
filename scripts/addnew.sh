b35d77184e815c42009896d15cdcd723cae15718
goldfinger <60414807+g0Idfinger@users.noreply.github.com>
2022-07-07 14:45:29 -0700

    Update addnew.sh

--- scripts/addnew.sh
+++ scripts/addnew.sh
@@ -1,58 +1,105 @@
 #!/bin/bash
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo ""
+   echo ""
+   echo "${green}Creates new instance of Velociraptor."
+   echo
+   echo "Syntax: ./addnew.sh [-c CUSTOMERNAME] [-g] [-p PASSWORD OF 30 CHARACTERS OR MORE]"
+   echo "options:"
+   echo "-c     name of customer"
+   echo "-g     Generates password"
+   echo "-p     Provide a password manually(must be 30 characters or more)"
+   echo
+   echo "./addnew.sh -c CUSTOMERNAME -p PASSWORD"
+   echo "or"
+   echo "./addnew.sh -c CUSTOMERNAME -g${reset}"
+}
+#checks for arguments
+g_used=false p_used=false
+while getopts ":h:c:gp:" option; do
+        case $option in
+                h) # display help
+                        Help
+                        exit;;
+                c) # customer name
+                        custname=${OPTARG,,};;
+                g) # generate password
+                        g_used=true
+                        val1=`shuf -i 10-99 -n1`
+                        val2=`shuf -i 10-99 -n1`
+                        xkpass=`xkpassword`
+                        passwd="$val1.$xkpass.$val2";;
+                        #echo "$passwd";;
+                p) # User input password
+                        p_used=true
+                        if [[ ${#OPTARG} -lt 30 ]]; then
+                                echo "${red}Password needs to be 30 characters or longer${reset}"
+                                exit
+                        else
+                                passwd=$OPTARG
+                        fi;;
+                \?) # invalid Option
+                        echo "${red}Error: invlaid option${reset}"
+                        exit;;
+        esac
+done
+if "$g_used" && "$p_used"; then
+        echo "${red}Use ether -g or -p  not both${reset}"
+        exit
+fi
+# Mandatory Arguments
+if [ ! "$custname" ] || [ ! "$passwd" ]; then
+        echo "${red}Arguments -c and -g or arguments -c and -p must be provided${reset}"
+        Help
+        exit 1
+fi
+
 if [ `whoami` = root ]; then
 	echo Please do not run this script as root or using sudo
 	exit
 fi
 # import .env variables
 set -o allexport; source ../.env; set +o allexport
-# asks for customer name and puts into a var custname
-read -p 'Deployment Name, used to be prefix to domain name: ' custnamel
-custname=${custnamel,,}
 #copies config to cust.txt
-mkdir ../containers/$custname-velo
-cp ../staging/newvelo.txt ../staging/$custname-velo.txt
+mkdir $DOCKERDIR/containers/$custname-velo
+cp $DOCKERDIR/staging/newvelo.txt DOCKERDIR/staging/$custname-velo.txt
 # replaces txt in file to complete config
-sed -i "s/CUSTOMER/$custname-velo/g" ../staging/$custname-velo.txt
+sed -i "s/CUSTOMER/$custname-velo/g" $DOCKERDIR/staging/$custname-velo.txt
 # copies dockerfile and entrypoint to customer config
-cp ../staging/Dockerfile ../containers/$custname-velo/
-cp ../staging/entrypoint ../containers/$custname-velo/
+cp $DOCKERDIR/staging/Dockerfile $DOCKERDIR/containers/$custname-velo/
+cp $DOCKERDIR/staging/entrypoint $DOCKERDIR/containers/$custname-velo/
 #adds the config file to the docker compose file
-cat ../staging/$custname-velo.txt >> ../docker-compose.yml
-#configures dns
-dnsrecord=$custname-velo.$DOMAINNAME
-cloudflare_auth_key=$CLOUDFLARE_TOKEN
-zoneid=$(curl -X GET "https://api.cloudflare.com/client/v4/zones" \
-  -H "Authorization: Bearer $CLOUDFLARE_TOKEN" \
-  -H "Content-Type: application/json" | jq -r  '{"result"}[] | .[0] | .id')
-# update the record
-curl -s PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" \
-  -H "Authorization: Bearer $cloudflare_auth_key" \
-  -H "Content-Type: application/json" \
-  --data "{\"type\":\"CNAME\",\"name\":\"$dnsrecord\",\"content\":\"$DOMAINNAME\",\"ttl\":1,\"proxied\":true}" | jq
+cat $DOCKERDIR/staging/$custname-velo.txt >> $DOCKERDIR/docker-compose.yml
 #brings up the new container to stage files
-cd ..
+cd $DOCKERDIR
 docker compose up -d
-#allows time for velo to build
-sleep 15
+#stop script while new container builds
+echo "${red} __     __     __ __           ___       "
+echo "${red}|__)|  |_  /\ (_ |_   |  | /\ | |        "
+echo "${red}|   |__|__/--\__)|__  |/\|/--\| |        "
+echo "${reset}                                       "
+sleep 60
 #stops new container
 docker stop $custname-velo
 #customizes velo config for customer info
-sed -i 's/bind_port: 8889/bind_port: 8889\n  use_plain_http: true/g' containers/$custname-velo/server.config.yaml
-sed -i 's/bind_port: 8000/bind_port: 8889\n  use_plain_http: true/g' containers/$custname-velo/server.config.yaml
-sed -i 's/use_self_signed_ssl: true/use_self_signed_ssl: false/g' containers/$custname-velo/server.config.yaml
-sed -i 's/use_self_signed_ssl: true/use_self_signed_ssl: false/g' containers/$custname-velo/client.config.yaml
+sed -i 's/bind_port: 8889/bind_port: 8889\n  use_plain_http: true/g' $DOCKERDIR/containers/$custname-velo/server.config.yaml
+sed -i 's/bind_port: 8000/bind_port: 8889\n  use_plain_http: true/g' $DOCKERDIR/containers/$custname-velo/server.config.yaml
+sed -i 's/use_self_signed_ssl: true/use_self_signed_ssl: false/g' $DOCKERDIR/containers/$custname-velo/server.config.yaml
+sed -i 's/use_self_signed_ssl: true/use_self_signed_ssl: false/g' $DOCKERDIR/containers/$custname-velo/client.config.yaml
 #starts new container
 docker compose up -d
-#clean up files
 rm staging/$custname-velo.txt
 #Create username
 read -p 'Velociraptor Admin username: ' velouser
-# Generate Password
-val1=`shuf -i 10-99 -n1`
-val2=`shuf -i 10-99 -n1`
-xkpass=`xkpassword`
-passwd="$val1.$xkpass.$val2"
 #sets username and password in velo DB
 docker exec -it $custname-velo ./velociraptor --config server.config.yaml user add $velouser $passwd --role administrator
 #prints username and password
-echo -e "\e[1;42m open browser and go to https://$custname-velo.$DOMAINNAME, username is $velouser password is $passwd \e[0m"
+echo "Open browser and go to ${red}https://$custname-velo.$DOMAINNAME${reset}, username is ${red}$velouser ${reset}password is ${red}$passwd${reset}"
